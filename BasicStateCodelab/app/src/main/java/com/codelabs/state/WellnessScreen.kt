df2761b1f8a06cb21415f0a33848a7d9e0e2422c
dpcks0509 <dpcks0509@gmail.com>
2025-01-15 16:06:54 +0900

    feat: State hoisting

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
 
 //@Composable
