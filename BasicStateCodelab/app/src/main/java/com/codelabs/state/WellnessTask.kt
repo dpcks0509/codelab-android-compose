b54fc7165fa86ab75ff1b79490602d7e122ac8bd
dpcks0509 <dpcks0509@gmail.com>
2025-01-15 17:00:57 +0900

    feat: State in ViewModel

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessTask.kt
@@ -19,7 +19,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-data class WellnessTask(val id: Int, val label: String)
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
 
 //class WellnessTask(
 //    val id: Int,
--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessViewModel.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WellnessViewModel.kt
@@ -18,6 +18,23 @@ package com.codelabs.state
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 
+class WellnessViewModel : ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(item: WellnessTask, checked: Boolean) =
+        _tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+}
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
 //class WellnessViewModel : ViewModel() {
 //    /**
 //     * Don't expose the mutable list of tasks from outside the ViewModel.
